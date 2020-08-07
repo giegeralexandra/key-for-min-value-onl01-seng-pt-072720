45bdd8e8a1d048e0ada5b7144150a2d47cf8f595
Alexandra Gieger <giegeralexandra@gmail.com>
2020-08-07 01:52:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   min_value_key = {"name" => 0 }
-  min_key
+  min_key = ""
   name_hash.each do |key,value|
     if value < min_value_key["name"]
       min_value_key = {"#{key}" => "#{value}"}
